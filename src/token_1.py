2e3d793b911c48673d470e4996ad14d9d2ea8fde
tzr897 <ziruitang897@gmail.com>
2022-11-19 12:57:40 -0800

    update

--- src/token_1.py
+++ src/token_1.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
@@ -6,21 +7,26 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 enron_data_path = "../dataset/enron1/"
 
-e1 = []
-for root, dirs, files in os.walk(enron_data_path):
-    for file_name in files:
-        cur_file_path = os.path.join(root, file_name)
-        cur_txt = ""
-        with open(cur_file_path) as cur_f:
-            try:
-                cur_lines = cur_f.readlines()
-                for one_line in cur_lines:
-                    one_line = one_line.strip('\n')
-                    one_line = one_line.strip('\r')
-                    cur_txt += one_line
-                e1.append(cur_txt)
-            except UnicodeDecodeError:
-                continue
+# return the list of preprocess email text
+def preprocess_one_set(data_path):
+    res = []
+    for root, dirs, files in os.walk(enron_data_path):
+        for file_name in files:
+            cur_file_path = os.path.join(root, file_name)
+            cur_txt = ""
+            with open(cur_file_path) as cur_f:
+                try:
+                    cur_lines = cur_f.readlines()
+                    for one_line in cur_lines:
+                        one_line = one_line.strip('\n')
+                        one_line = one_line.strip('\r')
+                        cur_txt += one_line
+                    e1.append(cur_txt)
+                except UnicodeDecodeError:
+                    continue
+    return res
 
-for i in range(0,10):
-    print(type(e1[i]))
+def process_all_data(data_path):
+    spam = []
+    ham = []
+    
