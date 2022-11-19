23e74fa273c31fb6c11699141b78cc7ccb091d29
tzr897 <ziruitang897@gmail.com>
2022-11-19 13:33:39 -0800

    update

--- src/token_1.py
+++ src/token_1.py
@@ -10,7 +10,7 @@ enron_data_path = "../dataset/enron1/"
 # return the list of preprocess email text
 def preprocess_one_set(data_path):
     res = []
-    for root, dirs, files in os.walk(enron_data_path):
+    for root, dirs, files in os.walk(data_path):
         for file_name in files:
             cur_file_path = os.path.join(root, file_name)
             cur_txt = ""
@@ -21,12 +21,24 @@ def preprocess_one_set(data_path):
                         one_line = one_line.strip('\n')
                         one_line = one_line.strip('\r')
                         cur_txt += one_line
-                    e1.append(cur_txt)
+                    res.append(cur_txt)
                 except UnicodeDecodeError:
                     continue
     return res
 
-def process_all_data(data_path):
-    spam = []
-    ham = []
-    
+def process_all_data():
+    spam_path = enron_data_path + "spam/"
+    ham_path = enron_data_path + "ham/"
+    spam_list = preprocess_one_set(spam_path)
+    ham_list = preprocess_one_set(ham_path)
+    print(len(spam_list))
+    print(spam_list[1])
+    print()
+    print(len(ham_list))
+    print(ham_list[1])
+
+def main():
+    process_all_data()
+
+if __name__ == "__main__":
+    main()
